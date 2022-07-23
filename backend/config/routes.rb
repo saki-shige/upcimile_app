5ddf226f5f092bff7ff1fef7f8cc628114791683
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-23 11:14:59 +0900

    [fix] sessionsコントローラーをdeviseをマウントするように修正
    -mストロングパラメーター設定のため

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # resources :test, only: %i[index]
 
       mount_devise_token_auth_for 'Company', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
+        registrations: 'api/v1/auth/registrations',
+        sessions: 'api/v1/auth/sessions'
       }
 
       namespace :auth do
