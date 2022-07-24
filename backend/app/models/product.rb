59c7b57b5e6bb501ff93cd8be5c60006d7eb0478
重田早紀 <8221sa@gmail.com>
2022-07-24 21:09:25 +0900

    [add] Product-Tag間のアソシエーション定義

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :company
   belongs_to :category
+  has_many :product_tags
+  has_many :tags, through: :product_tags
 end
--- backend/app/models/product_tag.rb
+++ backend/app/models/product_tag.rb
@@ -1,2 +1,4 @@
 class ProductTag < ApplicationRecord
+  belongs_to :product
+  belongs_to :tag
 end
