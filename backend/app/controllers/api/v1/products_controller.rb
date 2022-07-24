1458fd42b7ff615e28591b16b8bf98f0438a6f2e
重田早紀 <8221sa@gmail.com>
2022-07-24 16:58:49 +0900

    [add] products#createアクション追加

--- backend/app/controllers/api/v1/products_controller.rb
+++ backend/app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,17 @@ class Api::V1::ProductsController < ApplicationController
     products = Product.all
     render json: products
   end
+
+  def create
+    product = Product.create(product_params)
+    if product.save
+      render json: {status:201}
+    else
+      render json: {status:400, message:'保存できませんでした'}
+    end
+  end
+
+  def product_params
+    params.permit(:name, :introduction, :available_from, :available_to, :can_be_provided, :company_id, :category_id)
+  end
 end
