b3863c57c0c2d0a6b2767076ed192fee5ad4f626
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-24 14:43:44 +0900

    [add] Homeでログインしているユーザー名を表示

--- frontend/app/src/components/pages/Home.tsx
+++ frontend/app/src/components/pages/Home.tsx
@@ -1,10 +1,16 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "react-router-dom";
 
+import { AuthContext } from "../providers/AuthProvider";
+
 const Home: React.FC = () => {
+  const { isSignedIn, currentCompany } = useContext(AuthContext);
+
   return (
     <>
       <p>Home</p>
+    {/* // (仮）サインインしているユーザー（カンパニー名）を表示 */}
+      <h2>{isSignedIn ? <p>{currentCompany.name}としてサインインしています</p> : <p>サインインしていません</p>}</h2>
       <Link to={'/signin'}>sign in</Link>
       <Link to={'/signup'}>sign up</Link>
     </>
