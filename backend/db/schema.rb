049def80882b9ab16af68ec17528a21548fa7b34
重田早紀 <8221sa@gmail.com>
2022-07-24 20:31:55 +0900

    [add] Ctegoryモデル追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_24_060541) do
+ActiveRecord::Schema.define(version: 2022_07_24_112913) do
+
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
