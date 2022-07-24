ba24f327298f55c552b6e52849a611b19db23b7f
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-24 15:29:05 +0900

    [add] Product-Company間の関連付け

--- backend/app/models/company.rb
+++ backend/app/models/company.rb
@@ -6,4 +6,5 @@ class Company < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :products, dependent: :destroy
 end
