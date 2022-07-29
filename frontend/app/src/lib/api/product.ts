afae469d8e066ac040cf201faebf2ddd17afead8
重田早紀 <8221sa@gmail.com>
2022-07-29 22:36:02 +0900

    [fix] product作成時のformdataの型指定

--- frontend/app/src/lib/api/product.ts
+++ frontend/app/src/lib/api/product.ts
@@ -1,4 +1,5 @@
 import client from "./client"
+import { UpdateProductFormData } from "../../interface"
 
 export const getProducts = () => {
   return client.get("products")
@@ -8,7 +9,7 @@ export const getSingleProduct = (id: string) => {
   return client.get(`products/${id}`);
 }
 
-export const createProduct = (data: any) => {
+export const createProduct = (data: UpdateProductFormData) => {
   return client.post("products", data, {headers: {
     'content-type': 'multipart/form-data',
   },
