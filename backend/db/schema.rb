f1bd385de9129bc834a41ac5da74d770c11299ec
重田早紀 <8221sa@gmail.com>
2022-07-29 09:16:38 +0900

    [add] productプロフィール情報カラムの追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_082059) do
+ActiveRecord::Schema.define(version: 2022_07_28_115531) do
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 2022_07_28_082059) do
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "introduction"
+    t.string "address"
+    t.integer "number_of_employees"
+    t.integer "capital"
+    t.date "date_of_establishment"
+    t.string "corporate_site"
     t.index ["confirmation_token"], name: "index_companies_on_confirmation_token", unique: true
     t.index ["email"], name: "index_companies_on_email", unique: true
     t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
