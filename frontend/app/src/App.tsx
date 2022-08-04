71cf97780db4b228d26e3fdbff693cd395030853
重田早紀 <8221sa@gmail.com>
2022-08-04 22:52:22 +0900

    [fix] CssBaselineを設定

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,5 +1,8 @@
 import React, { createContext } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
 import ResponsiveAppBar from './components/layouts/ResponsiveAppBar';
 import Home from './components/pages/Home';
 import SignIn from './components/pages/SignIn';
@@ -15,21 +18,24 @@ import CreatorLogin from './components/pages/CreatorLogin';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ResponsiveAppBar />
-      <Routes>
-        <Route path='/' element={<Home />} />;
-        <Route path='/signin' element={<SignIn />} />;
-        <Route path='/signup' element={<SignUp />} />;
-        <Route path='/products' element={<Products />} />;
-        <Route path='/products/*' element={<SingleProduct />} />;
-        <Route path='/products/new' element={<CreateProduct />} />;
-        <Route path='/companies' element={<Companies />} />;
-        <Route path='/companies/*' element={<SingleCompany />} />;
-        <Route path='/companies/edit/:id' element={<EditCompany />} />;
-        <Route path='/creators/login' element={<CreatorLogin />} />;
-      </Routes>
-    </BrowserRouter>
+    <>
+    <CssBaseline />
+      <BrowserRouter>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path='/' element={<Home />} />;
+          <Route path='/signin' element={<SignIn />} />;
+          <Route path='/signup' element={<SignUp />} />;
+          <Route path='/products' element={<Products />} />;
+          <Route path='/products/*' element={<SingleProduct />} />;
+          <Route path='/products/new' element={<CreateProduct />} />;
+          <Route path='/companies' element={<Companies />} />;
+          <Route path='/companies/*' element={<SingleCompany />} />;
+          <Route path='/companies/edit/:id' element={<EditCompany />} />;
+          <Route path='/creators/login' element={<CreatorLogin />} />;
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
