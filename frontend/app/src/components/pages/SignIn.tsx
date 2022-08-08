d98a520c2fbaf28e71fa4dbf994d57868980cd04
重田早紀 <8221sa@gmail.com>
2022-08-08 17:26:29 +0900

    [fix] 不要な記述を削除、アイコンを中央寄せ

--- frontend/app/src/components/pages/SignIn.tsx
+++ frontend/app/src/components/pages/SignIn.tsx
@@ -13,14 +13,9 @@ import { signIn } from "../../lib/api/auth"
 import { AuthContext } from "../providers/AuthProvider";
 
 import Avatar from '@mui/material/Avatar';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-
-const theme = createTheme();
 
 export default function SignIn() {
   const [email, setEmail] = useState<string>("")
@@ -61,21 +56,19 @@ export default function SignIn() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
+      <Container component="main" maxWidth="xs" sx={{mb:10}}>
         <Box
           sx={{
-            marginTop: 8,
+            m: 20,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ my: 3, mx: 'auto', bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component="h1" variant="h5" style={{ textAlign: 'center'}}>
             Sign in
           </Typography>
           <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
@@ -119,6 +112,5 @@ export default function SignIn() {
           </Box>
         </Box>
       </Container>
-    </ThemeProvider>
   );
 }
