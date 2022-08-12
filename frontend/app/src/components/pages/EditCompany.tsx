ba6752f86558a108163f833bcf525c6319e7c052
重田早紀 <8221sa@gmail.com>
2022-08-12 17:23:56 +0900

    [fix] 会社情報編集画面を商品登録・編集画面に合わせる

--- frontend/app/src/components/pages/EditCompany.tsx
+++ frontend/app/src/components/pages/EditCompany.tsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { Button } from "@mui/material";
+import { Button, Grid, TextField, Container, Paper, Typography, Box } from "@mui/material";
 
-import { Company } from "../../interface";
-import { UpdateCompanyData } from "../../interface";
+import ImageForm from "../layouts/ImageForm";
 import { getSingleCompany, updateCompany } from "../../lib/api/company";
 
 
 type FormData = {
-  name: string
-  image?: File
+  name?: string
   introduction?: string
   address?: string
   numberOfEmployees?: number
@@ -20,11 +18,12 @@ type FormData = {
 }
 
 const EditCompany: React.FC = () => {
-  const [ company, setCompany ] = useState<FormData>({
-    name: '',
-  });
+  const [ company, setCompany ] = useState<FormData>();
+  const [croppedFile, setCroppedFile] = useState<File | null>(null);
+  const [ preImage, setPreImage] = useState<string>();
   const navigation = useNavigate();;
   const {id} = useParams<{id: string}>();
+  const [ changeImage, setChangeImage ] = useState<boolean>(false);
 
   const handleGetCompany = async(id:string) => {
     try {
@@ -34,6 +33,7 @@ const EditCompany: React.FC = () => {
       if (res.status === 200) {
         console.log('商品詳細を取得しました')
         setCompany(res.data)
+        setPreImage(res.data.image.url)
       } else {
         console.log("No products")
       }
@@ -47,7 +47,7 @@ const EditCompany: React.FC = () => {
     id && handleGetCompany(id)
   },[]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     if (e.target.name === 'image' && e.target.files){
       setCompany({...company, [name]: e.target.files[0]});
@@ -60,14 +60,16 @@ const EditCompany: React.FC = () => {
     e.preventDefault()
 
     const formData = new FormData();
-      formData.append("company[name]", company.name);
-      formData.append("company[image]", company.image || "");
+    if(company){
+      formData.append("company[name]", company.name || "");
+      changeImage && (formData.append("company[image]", croppedFile || ""));
       formData.append("company[introduction]", company.introduction || "");
       formData.append("company[address]", company.address || "");
       formData.append("company[numberOfEmployees]", String(company.numberOfEmployees));
       formData.append("company[capital]", String(company.capital));
       formData.append("company[dateOfEstablishment]", String(company.dateOfEstablishment) || "");
       formData.append("company[corporateSite]", String(company.corporateSite) || "");
+    }
 
     if(id){
       try {
@@ -91,41 +93,157 @@ const EditCompany: React.FC = () => {
 
   return(
     <>
-    { company ? (
-    <form>
-      <input type="file" accept="image/*,.png,.jpg,.jpeg,.gif" name="image" id="image" onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)} />
-      <div>
-        <label htmlFor="name">会社名：</label>
-        <input type="text" name="name" id="name" onChange={(e) => handleChange(e)} value={company.name}/>
-      </div>
-      <div>
-        <label htmlFor="introduction">会社概要：</label>
-        <input type="text" name="introduction" id="introduction" onChange={(e) => handleChange(e)} value={company.introduction}/>
-      </div>
-      <div>
-        <label htmlFor="address">住所：</label>
-        <input type="text" name="address" id="address" onChange={(e) => handleChange(e)} value={company.address}/>
-      </div>
-      <div>
-        <label htmlFor="numberOfEmployees">従業員数：</label>
-        <input type="number" name="numberOfEmployees" id="numberOfEmployees" onChange={(e) => handleChange(e)} value={company.numberOfEmployees}/>
-      </div>
-      <div>
-        <label htmlFor="capital">資本金：</label>
-        <input type="number" name="capital" id="capital" onChange={(e) => handleChange(e)} value={company.capital}/>
-      </div>
-      <div>
-        <label htmlFor="dateOfEstablishment">設立：</label>
-        <input type="date" name="dateOfEstablishment" id="dateOfEstablishment" onChange={(e) => handleChange(e)} value={String(company.dateOfEstablishment)}/>
-      </div>
-      <div>
-        <label htmlFor="corporateSite">会社HP : </label>
-        <input type="text" name="corporateSite" id="corporateSite" onChange={(e) => handleChange(e)} value={company.corporateSite}/>
-      </div>
-      <Button variant="outlined" type="submit" onClick={handleFormData}>会社情報を更新する</Button>
-    </form>
-    ):''}
-  </>
+    {company && (
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <Typography component="h1" variant="h4" align="center">
+            商品情報編集
+          </Typography>
+          <Typography variant="h6" gutterBottom sx={{textAlign: 'center'}}>
+            商品画像
+          </Typography>
+          <Grid container spacing={3}>
+            {changeImage ? (
+              <>
+              <Grid item xs={12}>
+                <ImageForm setCroppedFile={setCroppedFile}/>
+              </Grid>
+              <Grid item xs={12} sx={{textAlign:'center'}}>
+                <Button onClick={()=>setChangeImage(false)}>元の画像に戻す</Button>
+              </Grid>
+              </>
+            ) :(
+              <>
+            <Grid item xs={12}>
+              <Paper
+                sx={{
+                  width: 400,
+                  height: 300,
+                  mx: 'auto',
+                  borderRadius: 0,
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'center',
+                  backgroundImage: `url(${preImage})`,
+                }}
+              >
+            </Paper>
+            </Grid>
+            <Grid item xs={12} sx={{textAlign:'center'}}>
+              <Button onClick={()=>setChangeImage(true)}>画像を変更する</Button>
+            </Grid>
+            </>
+            )}
+            <Grid item xs={12}>
+              <TextField
+                required
+                name = "name"
+                label="名前"
+                fullWidth
+                defaultValue={company.name}
+                autoComplete="given-name"
+                variant="standard"
+                onChange={handleOnChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                name="introduction"
+                label="説明"
+                multiline
+                minRows={3}
+                maxRows={4}
+                fullWidth
+                defaultValue={company.introduction}
+                variant="standard"
+                onChange={handleOnChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                name="address"
+                label="住所"
+                multiline
+                minRows={3}
+                maxRows={4}
+                fullWidth
+                defaultValue={company.address}
+                variant="standard"
+                onChange={handleOnChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                type='number'
+                name="numberOfEmployees"
+                label="従業員数"
+                fullWidth
+                defaultValue={company.numberOfEmployees}
+                variant="standard"
+                onChange={handleOnChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                type='number'
+                name="capital"
+                label="資本金"
+                fullWidth
+                defaultValue={company.capital}
+                variant="standard"
+                onChange={handleOnChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                type='date'
+                name='dateOfEstablishment'
+                label="設立年月日"
+                fullWidth
+                defaultValue={company.dateOfEstablishment}
+                variant="standard"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                onChange={handleOnChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                name="corporateSite"
+                label="会社HP"
+                multiline
+                minRows={3}
+                maxRows={4}
+                fullWidth
+                defaultValue={company.corporateSite}
+                variant="standard"
+                onChange={handleOnChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  onClick={handleFormData}
+                  sx={{ mt: 3, ml: 1 }}
+                >
+                  更新する
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+          </Paper>
+      </Container>
+      )};
+    </>
   );
 };
 
