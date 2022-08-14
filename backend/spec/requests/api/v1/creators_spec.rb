fd587e00fb338813dce2d39b6d0ca5da58c6041d
重田早紀 <8221sa@gmail.com>
2022-08-14 23:45:02 +0900

    [fix] response.bodyのテストをしていなかったので追加

--- backend/spec/requests/api/v1/creators_spec.rb
+++ backend/spec/requests/api/v1/creators_spec.rb
@@ -13,12 +13,14 @@ RSpec.describe "Companies", :type => :request do
 
     context 'モデルからデータが取得できた時' do
       let(:creator_info){ {channel_title: 'test_channel_title'} }
-      let(:creator_videos){ [{channel_title: 'test_channel_title'}] }
+      let(:creator_videos){ [{video_title: 'test_channel_title'}] }
 
-      it 'status200とcreator_info、creator_videosを返す' do
+      it 'creator_info、creator_videosを返す' do
         get "/api/v1/creators/#{creator.id}"
         expect(response.status).to eq(200)
         json = JSON.parse(response.body)
+        expect(json['creator_info']['channel_title']).to eq(creator_info[:channel_title])
+        expect(json['creator_videos'][0]['video_title']).to eq(creator_videos[0][:video_title])
       end
     end
 
@@ -35,16 +37,16 @@ RSpec.describe "Companies", :type => :request do
 
   describe 'GET /api/v1/creators/' do
     before do
-      youtube_client_mock = instance_double('Youtube client')
-      allow(Creator).to receive(:list_up_all_channel_info).and_return(youtube_client_mock)
-      allow(youtube_client_mock).to receive(:list_up_all_channel_info).and_return(creators_info)
+      allow(Creator).to receive(:list_up_all_channel_info).and_return(creators_info)
     end
     context 'モデルからデータが取得できた時' do
       let(:creators_info){ [{channel_title: 'test_channel_title'}] }
 
-      it 'status200とcreator_info、creator_videosを返す' do
+      it 'creators_infoを返す' do
         get "/api/v1/creators"
         expect(response.status).to eq(200)
+        json = JSON.parse(response.body)
+        expect(json[0]['channel_title']).to eq(creators_info[0][:channel_title])
       end
     end
 
