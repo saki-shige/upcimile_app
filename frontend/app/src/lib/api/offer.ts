ffaadf12e1a9c8c9349d66255d3e6b4ccfa1f80b
重田早紀 <8221sa@gmail.com>
2022-08-19 22:07:13 +0900

    [add] offer httpリクエストのためのcomponentを作成

---
+++ frontend/app/src/lib/api/offer.ts
@@ -0,0 +1,33 @@
+import client from "./client"
+import Cookies from "js-cookie"
+import axios from "axios"
+
+export const makeOffer = (data: {idToken: string, product_id:string})  => {
+  return client.post('/offers', data)
+}
+
+export const respondToOffer = (id:string, type:'accept' | 'decline') => {
+  return client.put(`/offers/${id}?type=${type}`, {}, {headers: {
+    "access-token": Cookies.get("_access_token") || '',
+    "client": Cookies.get("_client") || '',
+    "uid": Cookies.get("_uid") || '',
+  },
+  });
+}
+
+export const getOffersToMe = () => {
+  return client.get('/offers?part=company', {headers: {
+    "access-token": Cookies.get("_access_token") || '',
+    "client": Cookies.get("_client") || '',
+    "uid": Cookies.get("_uid") || '',
+  },
+  });
+}
+
+export const getMyOffers = (data: {idToken: string}) => {
+return axios.get('http://localhost:3001/api/v1/offers?part=creator', {
+  headers: {
+    Authorization: `Bearer ${data.idToken}`,
+  }
+});
+}
