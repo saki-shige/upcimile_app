1da581a0efe46dec085567837154673e64dd9fee
重田早紀 <8221sa@gmail.com>
2022-08-19 21:35:49 +0900

    [fix] offerの操作（作成や承諾など)にユーザー認証状況のを確認

--- backend/app/controllers/api/v1/offers_controller.rb
+++ backend/app/controllers/api/v1/offers_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::OffersController < ApplicationController
+  before_action :authenticate_company, only: :accept
+  before_action :authenticate_creator, only: [:create]
+
   def index
-    if params[:creator_id]
-      offers = Offer.where(creator_id: params[:creator_id])
-      render status: 200, json: offers, include: [:product]
-    elsif params[:company_id]
-      offers = Offer.joins(:product).where(product: {company_id: params[:company_id]})
+    if params[:part] === 'creator'
+      id = current_api_v1_creator.id
+      offers = Offer.where(creator_id: id)
+      render status: 200, json: offers, :include => [product: { include: :company}]
+    elsif params[:part] === 'company'
+      id = current_api_v1_company.id
+      offers = Offer.joins(:product).where(product: {company_id: id})
       render status: 200, json: offers, include: [:product, :creator]
     else
       render status: 400, json: {message: 'company_idもしくはcreator_idを指定してください'}
@@ -13,6 +18,7 @@ class Api::V1::OffersController < ApplicationController
 
   def create
     offer = Offer.new(offer_params)
+    offer.creator_id = current_api_v1_creator.id
     if offer.save
       render status: 201, json: offer
     else
@@ -22,7 +28,7 @@ class Api::V1::OffersController < ApplicationController
 
   def accept
     offer = Offer.find(params[:id])
-    if offer.update(is_accepted: true)
+    if offer.update(is_accepted: params[:type] === 'accept' ? true : false)
       render status: 200, json: offer
     else
       render status: 500, json: {message: 'offerを承諾できませんでした'}
@@ -32,6 +38,6 @@ class Api::V1::OffersController < ApplicationController
   private
 
   def offer_params
-    params.require(:offer).permit(:product_id, :creator_id)
+    params.require(:offer).permit(:product_id)
   end
 end
