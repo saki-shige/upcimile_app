6b01efe6fe152e0cec8fe58beea67d1d0586c73f
重田早紀 <8221sa@gmail.com>
2022-08-20 12:55:55 +0900

    [fix] コンパクト形式に統一

--- backend/app/controllers/api/v1/auth/creators_controller.rb
+++ backend/app/controllers/api/v1/auth/creators_controller.rb
@@ -1,34 +1,28 @@
-module Api
-  module V1
-    module Auth
-      class CreatorsController < ApplicationController
-        require 'google/apis/youtube_v3'
+class Api::V1::Auth::CreatorsController < ApplicationController
+  require 'google/apis/youtube_v3'
 
-        def create
-          FirebaseIdToken::Certificates.request
-          raise ArgumentError, 'BadRequest Parameter' if payload.blank?
+  def create
+    FirebaseIdToken::Certificates.request
+    raise ArgumentError, 'BadRequest Parameter' if payload.blank?
 
-          creator = Creator.find_or_initialize_by(uid: payload['sub'])
-          creator.email = payload['email']
-          my_channel_info = creator.find_my_channel_info(params[:access_token])
-          raise ArgumentError, 'No Youtube Account' unless my_channel_info.items
+    creator = Creator.find_or_initialize_by(uid: payload['sub'])
+    creator.email = payload['email']
+    my_channel_info = creator.find_my_channel_info(params[:access_token])
+    raise ArgumentError, 'No Youtube Account' unless my_channel_info.items
 
-          creator.channel_id = my_channel_info.items[0].id
-          creator.name = my_channel_info.items[0].snippet.title
-          creator.introduction = my_channel_info.items[0].snippet.description
-          creator.image = my_channel_info.items[0].snippet.thumbnails.high.url
-          message = if creator.new_record?
-                      'ユーザー登録を完了しました'
-                    else
-                      'ログインしました'
-                    end
-          if creator.save
-            render json: { creator_info: creator, message: }
-          else
-            render json: creator.errors
-          end
-        end
-      end
+    creator.channel_id = my_channel_info.items[0].id
+    creator.name = my_channel_info.items[0].snippet.title
+    creator.introduction = my_channel_info.items[0].snippet.description
+    creator.image = my_channel_info.items[0].snippet.thumbnails.high.url
+    message = if creator.new_record?
+                'ユーザー登録を完了しました'
+              else
+                'ログインしました'
+              end
+    if creator.save
+      render json: { creator_info: creator, message: }
+    else
+      render json: creator.errors
     end
   end
 end
