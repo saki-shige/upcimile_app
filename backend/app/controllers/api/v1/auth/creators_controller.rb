1eb41c9d55679426e869f1385fbda3a83801d140
重田早紀 <8221sa@gmail.com>
2022-08-20 12:44:35 +0900

    [add] インデント、空欄の修正

--- backend/app/controllers/api/v1/auth/creators_controller.rb
+++ backend/app/controllers/api/v1/auth/creators_controller.rb
@@ -7,21 +7,23 @@ module Api
         def create
           FirebaseIdToken::Certificates.request
           raise ArgumentError, 'BadRequest Parameter' if payload.blank?
+
           creator = Creator.find_or_initialize_by(uid: payload['sub'])
           creator.email = payload['email']
           my_channel_info = creator.find_my_channel_info(params[:access_token])
           raise ArgumentError, 'No Youtube Account' unless my_channel_info.items
+
           creator.channel_id = my_channel_info.items[0].id
           creator.name = my_channel_info.items[0].snippet.title
           creator.introduction = my_channel_info.items[0].snippet.description
           creator.image = my_channel_info.items[0].snippet.thumbnails.high.url
-          if creator.new_record?
-            message = 'ユーザー登録を完了しました'
-          else
-            message = 'ログインしました'
-          end
+          message = if creator.new_record?
+                      'ユーザー登録を完了しました'
+                    else
+                      'ログインしました'
+                    end
           if creator.save
-            render json: { creator_info: creator, message: message }
+            render json: { creator_info: creator, message: }
           else
             render json: creator.errors
           end
