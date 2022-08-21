205bb374039f2cec9abd153db54c1fccc6272607
重田早紀 <8221sa@gmail.com>
2022-08-21 11:38:05 +0900

    [add] creator認証用のスタブを作成

--- backend/spec/support/authorization_spec.rb
+++ backend/spec/support/authorization_spec.rb
@@ -3,4 +3,12 @@ module AuthorizationHelper
     post '/api/v1/auth/sign_in', params: { session: { email: user.email, password: user.password } }
     response.headers.slice('client', 'uid', 'token-type', 'access-token')
   end
+
+  def stub_current_api_v1_creator(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_api_v1_creator).and_return(user)
+  end
+
+  def stub_authenticate_creator
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_creator).and_return(nil)
+  end
 end
