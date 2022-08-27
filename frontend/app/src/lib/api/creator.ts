4ed08ebfcc4f09d02ffd2700c2d8aaf8ffa4ef12
重田早紀 <8221sa@gmail.com>
2022-08-27 22:30:54 +0900

    [fix] ダブルクオートをシングルクオートに修正

--- frontend/app/src/lib/api/creator.ts
+++ frontend/app/src/lib/api/creator.ts
@@ -1,4 +1,4 @@
-import client from "./client"
+import client from './client'
 
 export const login = (data: {idToken: string, accessToken: string}) => {
   return client.post('/auth/creators', data)
