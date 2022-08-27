b9c1ce15cd16de843e0f26fca5a3d2426bc97f64
重田早紀 <8221sa@gmail.com>
2022-08-27 22:30:12 +0900

    [fix] 不要な;を削除

--- frontend/app/src/lib/api/creator.ts
+++ frontend/app/src/lib/api/creator.ts
@@ -2,12 +2,12 @@ import client from "./client"
 
 export const login = (data: {idToken: string, accessToken: string}) => {
   return client.post('/auth/creators', data)
-};
+}
 
 export const getCreators = () => {
   return client.get('/creators')
 }
 
 export const getSingleCreator = (id: string) => {
-  return client.get(`creators/${id}`);
-};
+  return client.get(`creators/${id}`)
+}
