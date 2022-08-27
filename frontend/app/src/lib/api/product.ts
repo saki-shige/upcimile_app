64e704368988d5e8951188a01b3ed8dbfa1c29ac
重田早紀 <8221sa@gmail.com>
2022-08-27 22:27:00 +0900

    [fix] インデント、"→の修正

--- frontend/app/src/lib/api/product.ts
+++ frontend/app/src/lib/api/product.ts
@@ -1,30 +1,43 @@
-import client from "./client"
-import Cookies from "js-cookie"
-import { UpdateProductFormData } from "../../interface"
+import client from './client'
+import Cookies from 'js-cookie'
+import { UpdateProductFormData } from '../../interface'
 
 export const getProducts = () => {
-  return client.get("products")
+  return client.get('products')
 }
 
 export const getSingleProduct = (id: string) => {
-  return client.get(`products/${id}`);
+  return client.get(`products/${id}`)
 }
 
 export const createProduct = (data: UpdateProductFormData) => {
-  return client.post("products", data, {headers: {
+  return client.post('products', data, { headers: {
     'content-type': 'multipart/form-data',
-    "access-token": Cookies.get("_access_token") || '',
-    "client": Cookies.get("_client") || '',
-    "uid": Cookies.get("_uid") || '',
-  },
-  });
+    'access-token': Cookies.get('_access_token') || '',
+    'client': Cookies.get('_client') || '',
+    'uid': Cookies.get('_uid') || '',
+  }
+  })
 }
+
 export const updateProduct = (id: string, params: any) => {
-  return client.patch(`products/${id}`, params, {headers: {
-    'content-type': 'multipart/form-data',
-    "access-token": Cookies.get("_access_token") || '',
-    "client": Cookies.get("_client") || '',
-    "uid": Cookies.get("_uid") || '',
-  },
-  });
+  return client.patch(`products/${id}`, params, {
+    headers: {
+      'content-type': 'multipart/form-data',
+      'access-token': Cookies.get('_access_token') || '',
+      'client': Cookies.get('_client') || '',
+      'uid': Cookies.get('_uid') || '',
+    }
+  })
+}
+
+export const deleteProduct = (id: string) => {
+  return client.delete(`products/${id}`, {
+    headers: {
+      'content-type': 'multipart/form-data',
+      'access-token': Cookies.get('_access_token') || '',
+      'client': Cookies.get('_client') || '',
+      'uid': Cookies.get('_uid') || '',
+    }
+  })
 }
