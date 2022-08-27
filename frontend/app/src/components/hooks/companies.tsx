bc74e853948bfb186cc93a871ca3576ef8ec1801
重田早紀 <8221sa@gmail.com>
2022-08-27 15:26:16 +0900

    [fix] 会社情報取得のためのhookを作成

---
+++ frontend/app/src/components/hooks/companies.tsx
@@ -0,0 +1,50 @@
+import { getCompanies, getSingleCompany } from '../../lib/api/company'
+import { useContext, useEffect, useState } from 'react'
+import { Company } from '../../interface'
+import { MessageContext } from '../providers/MessageProvider'
+
+export const useHandleGetCompanies: () => Company[] | undefined = () => {
+  const { setOpen, setMessage, setSeverity } = useContext(MessageContext)
+  const [companies, setCompanies] = useState<Company[]>()
+
+  useEffect(() => {
+    getCompanies()
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('ユーザー一覧を取得しました')
+          setCompanies(res.data)
+        } else {
+          throw new Error()
+        }
+      }).catch(() => {
+        setOpen(true)
+        setMessage('ユーザー情報を取得できませんでした。')
+        setSeverity('error')
+      })
+  }, [])
+
+  return (companies)
+}
+
+export const useHandleGetSingleCompany: (id: string) => Company | undefined = (id) => {
+  const { setOpen, setMessage, setSeverity } = useContext(MessageContext)
+  const [company, setCompany] = useState<Company>()
+
+  useEffect(() => {
+    getSingleCompany(id)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('ユーザー情報を取得しました')
+          setCompany(res.data)
+        } else {
+          throw new Error()
+        }
+      }).catch(() => {
+        setOpen(true)
+        setMessage('ユーザー情報を取得できませんでした。')
+        setSeverity('error')
+      })
+  }, [])
+
+  return (company)
+}
