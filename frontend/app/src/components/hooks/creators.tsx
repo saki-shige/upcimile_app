7a497bc29ed235dd51920b175f7c5057fc41cfea
重田早紀 <8221sa@gmail.com>
2022-08-27 15:26:49 +0900

    [fix] クリエイター情報取得のためのhookを作成

---
+++ frontend/app/src/components/hooks/creators.tsx
@@ -0,0 +1,52 @@
+import { getCreators, getSingleCreator } from '../../lib/api/creator'
+import { useContext, useEffect, useState } from 'react'
+import { Creator, Video } from '../../interface'
+import { MessageContext } from '../providers/MessageProvider'
+
+export const useHandleGetCreators: () => Creator[] | undefined = () => {
+  const { setOpen, setMessage, setSeverity } = useContext(MessageContext)
+  const [creators, setCreators] = useState<Creator[]>()
+
+  useEffect(() => {
+    getCreators()
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('ユーザー一覧を取得しました')
+          setCreators(res.data)
+        } else {
+          throw new Error()
+        }
+      }).catch(() => {
+        setOpen(true)
+        setMessage('ユーザー情報を取得できませんでした。')
+        setSeverity('error')
+      })
+  }, [])
+
+  return (creators)
+}
+
+export const useHandleGetSingleCreator: (id: string) => {creator: Creator | undefined, videos: Video[] | undefined} = (id) => {
+  const { setOpen, setMessage, setSeverity } = useContext(MessageContext)
+  const [creator, setCreator] = useState<Creator>()
+  const [videos, setVideos] = useState<Video[]>()
+
+  useEffect(() => {
+    getSingleCreator(id)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('ユーザー一覧を取得しました')
+          setCreator(res.data.creatorInfo)
+          setVideos(res.data.videos)
+        } else {
+          throw new Error()
+        }
+      }).catch(() => {
+        setOpen(true)
+        setMessage('ユーザー情報を取得できませんでした。')
+        setSeverity('error')
+      })
+  }, [])
+
+  return { creator, videos }
+}
