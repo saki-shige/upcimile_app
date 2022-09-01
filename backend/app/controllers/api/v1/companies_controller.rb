b03824e65816f858af60a32e7d4f6099d802ce0a
重田早紀 <8221sa@gmail.com>
2022-09-01 17:25:27 +0900

    [add] 一覧取得時に最新の（最近登録された）企業から取得

--- backend/app/controllers/api/v1/companies_controller.rb
+++ backend/app/controllers/api/v1/companies_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CompaniesController < ApplicationController
   before_action :authenticate_company, only: :update
 
   def index
-    companies = Company.all.limit(params[:limit])
+    companies = Company.recent.limit(params[:limit])
     render json: companies
   end
 
