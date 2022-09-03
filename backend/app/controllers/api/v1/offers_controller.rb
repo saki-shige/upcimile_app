a5254ff33616eebf6af8f588fb93e08ce3721d7d
重田早紀 <8221sa@gmail.com>
2022-09-03 12:21:25 +0900

    [fix] 行数が多すぎた点を修正

--- backend/app/controllers/api/v1/offers_controller.rb
+++ backend/app/controllers/api/v1/offers_controller.rb
@@ -7,14 +7,12 @@ class Api::V1::OffersController < ApplicationController
     when 'creator'
       return render status: 401, json: { message: 'ログインしてください' } unless current_api_v1_creator
 
-      id = current_api_v1_creator.id
-      offers = Offer.where(creator_id: id).recent
+      offers = Offer.where(creator_id: current_api_v1_creator.id).recent
       render status: 200, json: offers, include: [product: { include: :company }]
     when 'company'
       return render status: 401, json: { message: 'ログインしてください' } unless current_api_v1_company
 
-      id = current_api_v1_company.id
-      offers = Offer.joins(:product).where(product: { company_id: id }).recent
+      offers = Offer.joins(:product).where(product: { company_id: current_api_v1_company.id }).recent
       render status: 200, json: offers, include: %i[product creator]
     else
       render status: 400, json: { message: 'company_idもしくはcreator_idを指定してください' }
