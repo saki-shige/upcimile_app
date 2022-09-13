3b0668d3c17987c0b036672a04c96c7875053d76
重田早紀 <8221sa@gmail.com>
2022-09-13 23:18:36 +0900

    [fix] typograpyのフォントサイズを変更

--- frontend/app/src/assets/theme/theme.ts
+++ frontend/app/src/assets/theme/theme.ts
@@ -16,6 +16,12 @@ export const appTheme = createTheme({
       background: 'linear-gradient(transparent 60%, #C8E0BD 30%)',
       fontWeight: 'bold',
       margin: 10
+    },
+    h6: {
+      fontSize: '1rem',
+      '@media (min-width:600px)': {
+        fontSize: '1.2rem'
+      }
     }
   }
 })
