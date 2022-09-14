03b10665b4cd038ab3813113b24b89dd1ffa3c2e
重田早紀 <8221sa@gmail.com>
2022-09-14 17:37:15 +0900

    [add] ログイン状態でマイページからリクエストを送った（mypage=true)場合はすべての商品を、それ以外では掲載期間内の商品を返す

--- backend/app/controllers/api/v1/companies_controller.rb
+++ backend/app/controllers/api/v1/companies_controller.rb
@@ -8,7 +8,13 @@ class Api::V1::CompaniesController < ApplicationController
 
   def show
     company = Company.find(params[:id])
-    render json: company, include: [:products]
+    products = if params[:mypage] && company.id == current_api_v1_company.id
+                 company.products
+               else
+                 company.products.available
+               end
+    company = company.attributes.merge({ products: })
+    render json: company
   end
 
   def update
